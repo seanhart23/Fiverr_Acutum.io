5273647bfd66d329e56f1e744cde99a45f83e40e
uneelabs <codyc@uneelabs.com>
2024-03-28 09:07:11 -0500

    Restore or the site

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,6 @@ router.get('/analytics', function(req, res){
     res.render('analytics');
 });
 
-router.get('/welcome', function(req, res){
-    res.render('welcome');
-});
-
 //---------- NODE Mailer Routes ----------//
 router.post('/contact', function(req, res){
 
